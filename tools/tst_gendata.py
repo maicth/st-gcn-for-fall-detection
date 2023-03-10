08a662920ebd62c9c61397805fe690a60e2edde4
DungDuong1002 <dungduong1591@gmail.com>
2023-03-11 01:27:15 +0700

    Create tst_gendata.py

---
+++ tools/tst_gendata.py
@@ -0,0 +1,98 @@
+import os
+import sys
+import pickle
+
+import argparse
+import numpy as np
+from numpy.lib.format import open_memmap
+
+from utils.tst_read_skeleton import read_xyz
+
+training_subjects = [1, 3, 5, 7, 9, 10, 11]
+max_body = 2
+num_joint = 25
+max_frame = 500
+toolbar_width = 30
+
+def print_toolbar(rate, annotation=''):
+    # setup toolbar
+    sys.stdout.write("{}[".format(annotation))
+    for i in range(toolbar_width):
+        if i * 1.0 / toolbar_width > rate:
+            sys.stdout.write(' ')
+        else:
+            sys.stdout.write('-')
+        sys.stdout.flush()
+    sys.stdout.write(']\r')
+
+
+def end_toolbar():
+    sys.stdout.write("\n")
+
+
+def gendata(data_path,
+            out_path,
+            benchmark='xview',
+            part='eval'):
+
+    sample_name = []
+    sample_label = []
+    for filename in os.listdir(data_path):
+        element_name = filename.split('-')
+        subject_id = int(element_name[0][4:])
+        isfall = int(element_name[1] == 'Fall')
+        istraining = (subject_id in training_subjects)
+
+        if part == 'train':
+            issample = istraining
+        elif part == 'val':
+            issample = not (istraining)
+        else:
+            raise ValueError()
+
+        if issample:
+            sample_name.append(filename)
+            sample_label.append(isfall)
+
+    with open('{}/{}_label.pkl'.format(out_path, part), 'wb') as f:
+        pickle.dump((sample_name, list(sample_label)), f)
+    # np.save('{}/{}_label.npy'.format(out_path, part), sample_label)
+
+    fp = open_memmap(
+        '{}/{}_data.npy'.format(out_path, part),
+        dtype='float32',
+        mode='w+',
+        shape=(len(sample_label), 3, max_frame, num_joint, max_body))
+
+    for i, filename in enumerate(sample_name):
+        print_toolbar(i * 1.0 / len(sample_label),
+                      '({:>5}/{:<5}) Processing {:>5}-{:<5} data: '.format(
+                          i + 1, len(sample_name), benchmark, part))
+        data = read_xyz(
+            os.path.join(data_path, filename), max_body=max_body, num_joint=num_joint)
+        print(data.shape)
+        fp[i, :, 0:data.shape[1], :, :] = data
+    end_toolbar()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='TST Fall Detection Data Converter.')
+    parser.add_argument(
+        '--data_path', default='D://AIP490_data//tst_fall_detection_copy')
+    parser.add_argument('--out_folder', default='data/tst_fall_detection')
+
+    benchmark = ['xsub']
+    part = ['train', 'val']
+    arg = parser.parse_args()
+
+    for b in benchmark:
+        for p in part:
+            out_path = os.path.join(arg.out_folder, b)
+            if not os.path.exists(out_path):
+                os.makedirs(out_path)
+            gendata(
+                arg.data_path,
+                out_path,
+                benchmark=b,
+                part=p)
