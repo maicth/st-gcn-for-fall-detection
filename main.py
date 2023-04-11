fbd5b676a1c84d982b1328cd73eeae1c684b8c2e
DungDuong1002 <dungduong1591@gmail.com>
2023-04-11 23:14:00 +0700

    add aip_demo

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ if __name__ == '__main__':
     # region register processor yapf: disable
     processors = dict()
     processors['recognition'] = import_class('processor.recognition.REC_Processor')
+    processors['aip_demo'] = import_class('processor.aip_demo.AIPdemo')
     processors['demo_old'] = import_class('processor.demo_old.Demo')
-    processors['demo'] = import_class('processor.demo_realtime.DemoRealtime')
+    processors['demo_realtime'] = import_class('processor.demo_realtime.DemoRealtime')
     processors['demo_offline'] = import_class('processor.demo_offline.DemoOffline')
     #endregion yapf: enable
 
